ffe077e28be555075a9a943fcc6116977c48afda
Rodrigo Felipe <rodrigogfelipe@hotmail.com>
2020-01-02 13:43:30 -0300

    Configurando visibilidade de atributos e metados

---
+++ src/aula_02/Aula02.java
@@ -0,0 +1,24 @@
+package aula_02;
+
+public class Aula02 {
+
+	public static void main(String[] args) {
+		
+		/*Declarando obj da classe Caneta*/
+		Caneta c1 = new Caneta();
+		
+		c1.modelo= "Bic cristal";
+		c1.cor= "Azul";
+		//c1.ponta= 0.5f;
+		c1.carga= 80;
+		//c1.tampada= false;
+		
+		/*Metados para imprimir na tela */
+		c1.tampar();
+		c1.status();
+		c1.rabiscar();
+		
+		
+	}
+
+}
